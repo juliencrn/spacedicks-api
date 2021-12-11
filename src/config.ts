71afd0c2d8e683c3550bed39cf93c41861893941
Julien <juliencaron@protonmail.com>
2021-12-11 16:45:47 +0100

    Update env

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,7 @@ export const isDev = NODE_ENV === "development"
 // Website
 export const SITE_URL = isDev
   ? "http://localhost:3000"
-  : "https://cryptodicks.vercel.app"
+  : "https://cryptodicks.netlify.app"
 
 // SVG gen. config
 export const buildSize = 96;
@@ -16,11 +16,13 @@ export const displaySize = 600;
 export const PORT = process.env.PORT || 3001;
 export const API_URL = isDev
   ? `http://localhost:${PORT}`
-  : "https://cryptodicks-api.herokuapp.com"
+  : "https://cryptodicks.herokuapp.com"
 
 // Web3 config
+// mainnet | mumbai
+const network = process.env.NETWORK || "mumbai"
 export const RPC_URL = isDev
   ? "http://localhost:8545"
-  : `https://${process.env.NETWORK}.infura.io/v3/${process.env.INFURA_TOKEN}`
+  : `https://polygon-${network}.infura.io/v3/${process.env.INFURA_TOKEN}`
 
 export const WEB3_STORAGE_TOKEN = process.env.WEB3_STORAGE_TOKEN
